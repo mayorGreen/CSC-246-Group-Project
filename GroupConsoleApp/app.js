e8177409e986bbce01c92bb0b9f996c1a1fa4d96
Jacob T <jtrickel@missouriwestern.edu>
2019-11-22 09:06:58 -0600

    Made more functionality

--- GroupConsoleApp/app.js
+++ GroupConsoleApp/app.js
@@ -11,7 +11,7 @@ function isInt(value) {
     return !isNaN(value) && (function (x) { return (x | 0) === x })(parseFloat(value))
 }
 
-function tip(bill, tipAmount) {
+function tipCalculate(bill, tipAmount) {
     return (bill * (tipAmount / 100)).toFixed(2)
 }
 
@@ -19,132 +19,99 @@ function splitBill(numDiners, bill) {
     return (bill /numDiners).toFixed(2)
 }
 
+function noTip() {
+    console.log("Alright cheapskate, thanks for dining.")
+    return process.exit(1)
+}
+
+function mistakeCall() {
+    console.log("Did you even eat here?")
+    return process.exit(1)
+}
+
+function exit() {
+    return process.exit(1)
+}
+
+function tipAmount(totalBill) {
+    rl.question('What percent tip would you like to leave? (type a whole number) ', (tipAmount) => {
+        console.log(`Received: ${tipAmount}`)
+        if (isInt(tipAmount) == true && tipAmount > 0) {
+            console.log('You owe $' + tipCalculate(totalBill, tipAmount) + ' in tip.')
+            return process.exit(1)
+        } else {mistakeCall()}
+    })
+}
+
 
     console.log('Type exit or press control C to quit')
 rl.question('Is the check to be split (yes/no)) ', (answer) => {
-        switch (answer) {
+    switch (answer.toLowerCase()) {
 
-            case 'yes':
-            case 'Yes':
-            case 'y':
-            case 'Y':
-                rl.question('How many diners are there? (give a whole numerical value) ', (numDiners) => {
-                    console.log(`Received: ${numDiners}`)
-                    
-                    if (isInt(numDiners) == true) {
-                        rl.question('What was the total bill? (give a numerical value with 2 decimals) ', (totalBill) => {
-                            console.log(`Received: ${totalBill}`)
-                            if (isNaN(totalBill) == false) {
-                                console.log('The split bill is ' + splitBill(numDiners, parseFloat(totalBill)) + ' between ' + numDiners + ' diners.')
+        case 'yes':
+        case 'y':
+            rl.question('How many diners are there? (give a whole numerical value) ', (numDiners) => {
+                console.log(`Received: ${numDiners}`)
 
+                if (isInt(numDiners) == true) {
+                    rl.question('What was the total bill? (give a numerical value with 2 decimals) ', (totalBill) => {
+                        console.log(`Received: ${totalBill}`)
+                        if (isNaN(totalBill) == false) {
+                            console.log('The split bill is ' + splitBill(numDiners, parseFloat(totalBill)) + ' between ' + numDiners + ' diners.')
 
-                                rl.question('Will you be leaving a tip (yes/no)) ', (answer) => {
-                                    switch (answer) {
+                            rl.question('Will you be leaving a tip (yes/no)) ', (answer) => {
+                                switch (answer.toLowerCase()) {
 
-                                        case 'yes':
-                                        case 'Yes':
-                                        case 'y':
-                                        case 'Y':
-                                            rl.question('What percent tip would you like to leave? (type a whole number) ', (tipAmount) => {
-                                                console.log(`Received: ${tipAmount}`)
-                                                if (isInt(tipAmount) == true && tipAmount > 0) {
-                                                    console.log('Each person owes a $' + tip(splitBill(numDiners, totalBill), tipAmount) + ' tip each or one person owes $' + tip(totalBill, tipAmount) + ' total tip.')
-                                                    return process.exit(1)
-                                                } else {
-                                                    console.log('Stop wasting our time.')
-                                                    return process.exit(1)
-                                                }
-                                            })
-                                            break
+                                    case 'yes':
+                                    case 'y':
+                                        rl.question('What percent tip would you like to leave? (type a whole number) ', (tipAmount) => {
+                                            console.log(`Received: ${tipAmount}`)
+                                            if (isInt(tipAmount) == true && tipAmount > 0) {
+                                                console.log('Each person owes a $' + tipCalculate(splitBill(numDiners, totalBill), tipAmount) + ' tip each or one person owes $' + tipCalculate(totalBill, tipAmount) + ' total tip.')
+                                                return process.exit(1)
+                                            } else { mistakeCall() }
+                                        })
+                                        break
+                                    case 'no':
+                                    case 'n': noTip()
+                                    case 'exit':
+                                        exit()
+                                    default: mistakeCall()
+                                }
+                            })
+                        } else { mistakeCall() }
+                    })
+                } else { mistakeCall() }
+            })
+            break
 
-                                        case 'no':
-                                        case 'No':
-                                        case 'n':
-                                        case 'N':
-                                            console.log('Alright cheapskate, thanks for dining')
-                                            return process.exit(1)
+        case 'no':
+        case 'n':
+            rl.question('What was the total bill? (give a numerical value with 2 decimals) ', (totalBill) => {
+                console.log(`Received: ${totalBill}`)
 
-                                        case 'exit':
-                                        case 'Exit':
-                                            return process.exit(1)
+                if (isNaN(totalBill) == false) {
+                    rl.question('Do you want to tip (yes/no) ', (answer) => {
+                        switch (answer.toLowerCase()) {
 
-                                        default:
-                                            console.log('Did you even eat here?')
-                                            return process.exit(1)
+                            case 'yes':
+                            case 'y':
+                                tipAmount(totalBill)
 
-                                    }
-                                })
-                            } else {
-                                console.log("Did you even eat here?")
-                                return process.exit(1)
-                            }
-                        })
-                    } else {
-                        console.log("Did you even eat here?")
-                        return process.exit(1)
+                            case 'no':
+                            case 'n': noTip()
 
-                    }
-                })
-                break
+                            case 'exit': exit()
 
-            case 'no':
-            case 'No':
-            case 'n':
-            case 'N':
-                rl.question('What was the total bill? (give a numerical value with 2 decimals) ', (totalBill) => {
-                    console.log(`Received: ${totalBill}`)
-                    
-                    if (isNaN(totalBill) == false) {
-                        rl.question('Do you want to tip (yes/no) ', (answer) => {
-                            switch (answer) {
+                            default:
+                                mistakeCall()
+                        }
+                    })
+                } else { mistakeCall() }
+            })
+            break;
 
-                                case 'yes':
-                                case 'Yes':
-                                case 'y':
-                                case 'Y':
-                                    rl.question('What percent tip would you like to leave? (type a whole number) ', (tipAmount) => {
-                                        console.log(`Received: ${tipAmount}`)
-                                        if (isInt(tipAmount) == true && tipAmount > 0) {
-                                            console.log('You owe $' + tip(totalBill, tipAmount) + ' in tip.')
-                                            return process.exit(1)
-                                        } else {
-                                            console.log('Stop wasting our time.')
-                                            return process.exit(1)
-                                        }
-                                    })
-                                    break
-
-                                case 'no':
-                                case 'No':
-                                case 'n':
-                                case 'N':
-                                    console.log('Alright cheapskate, thanks for dining')
-                                    return process.exit(1)
-
-                                case 'exit':
-                                case 'Exit':
-                                    return process.exit(1)
-
-                                default:
-                                    console.log('Did you even eat here?')
-                                    break
-                                    return process.exit(1)
-                            }
-                        })
-                    } else {
-                        console.log("Did you even eat here?")
-                        return process.exit(1)
-                    }
-                })
-                break;
-
-            case 'exit':
-            case 'Exit':
-                return process.exit(1)
-
-            default:
-                console.log('Did you even eat here.')
-                return process.exit(1)
-
-        }
-    })
+        case 'exit': exit()
+        default: mistakeCall()
+    }
+})
